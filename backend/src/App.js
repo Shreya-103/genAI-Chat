6aff1f922ae39a92329c2a5952fdfa677c5c1a50
Shreya-103 <shreyaalpha1@gmail.com>
2026-09-07 13:20:41 +0530

    hope this model work

--- backend/src/App.js
+++ backend/src/App.js
@@ -16,7 +16,7 @@ app.post("/api/ai", async (req, res)=>{
     try{
         const {prompt} = req.body;
         const response = await ai.models.generateContent({
-            model: "gemini-3.8-flash",
+            model: "gemini-2.5-flash",
             contents: prompt
         });
         res.json({
